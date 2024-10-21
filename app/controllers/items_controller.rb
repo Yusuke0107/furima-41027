77b95dc78042afe9653de79c6b395471a60d7cbd
Yusuke0107 <yuusuke.n.1994@gmail.com>
2024-10-21 14:46:55 +0900

    商品出品機能の実装  修正④

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,14 +14,13 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :new, locals: { model: @item }, status: :unprocessable_entity
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shopping_cost_id,
-                                 :shopping_origin_id, :shopping_day_id, :price)
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shopping_cost_id,:shopping_origin_id, :shopping_day_id, :price)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_many :item
+  has_many :items
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
