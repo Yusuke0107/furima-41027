bcc4c90dfc5704e12a53c86cd4ce2a35e79b5145
Yusuke0107 <yuusuke.n.1994@gmail.com>
2024-10-25 15:02:38 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :ensure_item_owner, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,10 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, locals: {model: @item}, status: :unprocessable_entity
+    end
   end
 
   private
@@ -30,5 +39,19 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shopping_cost_id,
                                  :shopping_origin_id, :shopping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def ensure_item_owner
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def authenticate_user!
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
